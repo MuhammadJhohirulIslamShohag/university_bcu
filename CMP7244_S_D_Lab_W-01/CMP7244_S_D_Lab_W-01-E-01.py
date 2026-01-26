af11d2b81899f7b7c11aa60897d52d29694c4188
MuhammadShohagIslam <muhammadshohagislam.software.e@gmail.com>
2026-01-26 14:43:36 +0000

    'done'

---
+++ CMP7244_S_D_Lab_W-01/CMP7244_S_D_Lab_W-01-E-01.py
@@ -0,0 +1,22 @@
+
+'''
+Ex:01
+Write a Python script that will calculate the square root of the equation B*B + 4 * A *
+C. (hints you can use three variables to store the values of A, B and C). Note: to
+calculate square root of a number x, you need to implement the following x**(1/2.0) 
+
+'''
+a = 2
+b=3
+c = 5
+x = b*b + 4 * a * c 
+print(x**(1/2.0))
+
+
+
+
+
+
+
+
+
---
+++ CMP7244_S_D_Lab_W-01/CMP7244_S_D_Lab_W-01-E-02.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 26 14:24:02 2026
+
+@author: muham
+"""
+a = 9
+b= 8
+c = 12
+d = 33
+e = 5
+
+totalVariableValue = a + b + c + d + e
+meanValue = totalVariableValue / 5 
+totalSquareDevisionVlaue = ((a - meanValue) ** 2) + ((b - meanValue) ** 2) + ((c - meanValue) ** 2) + ((d - meanValue) ** 2) + ((e - meanValue) ** 2) 
+totalSquareDevisionVlaueSum = totalSquareDevisionVlaue / 5
+
+standardDeviationVlaue = totalSquareDevisionVlaueSum ** (1 / 2)
+print(standardDeviationVlaue)
