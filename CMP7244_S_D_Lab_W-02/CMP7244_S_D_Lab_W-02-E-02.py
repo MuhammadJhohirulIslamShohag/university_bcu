49968e792a6f2ebb41ed23cc7555925be5b68c51
MuhammadShohagIslam <muhammadshohagislam.software.e@gmail.com>
2026-02-02 13:57:58 +0000

    'done'

--- CMP7244_S_D_Lab_W-02/CMP7244_S_D_Lab_W-02-E-02.py
+++ CMP7244_S_D_Lab_W-02/CMP7244_S_D_Lab_W-02-E-02.py
@@ -11,6 +11,7 @@ the bmi_cal function
 
 '''
 
+"""
 def bmi_cal(name, weight, height):
     bmi = weight / (height * height)
     if(bmi < 18.5):
@@ -23,3 +24,79 @@ def bmi_cal(name, weight, height):
         return f'{name} is obese';
 
 print(bmi_cal('Muhammad', 52, 12));
+
+"""
+
+'''
+1.2 Converting numbers as strings into integers and floats
+print(int('10'))
+'''
+
+"""
+value = input('Enter the value of number:')
+print(float(value))
+
+"""
+
+"""
+# Converting single number value in without decimals value
+stringNumberValue = '10';
+print(int(stringNumberValue));
+
+"""
+
+"""
+1.3 Converting between floats and ints
+
+"""
+"""
+floatNumberValue = 9.6
+print(int(floatNumberValue))
+print(float(8))
+print(8*1.0) # Use of the float() function is preferred, because then the source code expresses more clearly what the programmer intended.
+"""
+
+"""
+1.4 Converting numbers into string format
+There are 2 approaches, direct conversion, or interpolation of numbers into strings. We can
+use the str() function to convert a number into string directly.
+"""
+print(str(8.5))
+print(str(3))
+
+#We can also interpolate numbers into strings. Python uses %d and %f for ints and floats.
+s = "an int %d number" % 10.5
+print(s)
+
+# We can also interpolate numbers into strings. Python uses %d and %f for ints and floats
+s = "a float %f number" % 3.3
+print(s)
+
+"""
+%f gives 6 decimal places. We can choose how many decimals we want. Formatting money
+amounts in a report we will typically want 2 decimal places.
+"""
+
+s = "a float %.1f number" % 3.3
+print(s)
+
+d = "a float %6.2f" % 123456789.01
+print(d)
+
+"""
+If we want to interpolate more than one number into a string, we can use a tuple, which is a
+comma separated set of values in round brackets, to supply the data.
+"""
+a=10
+b=3.6
+print("a float %f and an int %d" % (b, a))
+
+"""
+2. Definition of simple Python functions with input and output
+
+"""
+def square(x):
+    return x * x
+
+print(square(10))
+
